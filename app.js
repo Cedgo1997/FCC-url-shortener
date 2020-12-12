6ec3a00b293be72b7175126b8b44105327c53537
César Gonzalez <cedgo1997@gmail.com>
2020-12-11 22:05:12 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const port = 3000;
+
+// Mongoose Connect
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+//Config
+
+app.use(cors());
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+// First Endpoint
+app.get('/', function (req, res) {
+	res.sendFile(process.cwd() + '/views/index.html');
+});
+
+//LISTENING
+app.listen(port, () => console.log('Listening on port: ' + port));
