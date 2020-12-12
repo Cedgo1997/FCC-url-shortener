a337519760aa68d24f28069101c8bbfbb841a7ba
César Gonzalez <cedgo1997@gmail.com>
2020-12-12 00:10:51 -0400

    url displayed on json,

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 const port = 3000;
@@ -15,12 +16,21 @@ mongoose.connect(process.env.MONGO_URI, {
 //Config
 
 app.use(cors());
-
 app.use('/public', express.static(`${process.cwd()}/public`));
+// to parse POST bodies
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // First Endpoint
 app.get('/', (req, res) => {
 	res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// POST
+app.post('/api/shorturl/new', (req, res) => {
+	const url = req.body.url;
+	res.json({
+		url: url,
+	});
 });
 
 //LISTENING
